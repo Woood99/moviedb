c79e2e8e5956c1e912360d2a337e03d8a63d2b91
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2022-06-19 22:47:01 +0300

    fix

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -4,23 +4,6 @@ import {
     isDesktop
 } from '../functions/check-viewport';
 
-
-// if (document.querySelector('.search') && !isMobile()) {
-//     const searchForm = document.querySelector('.search');
-//     const searchInput = document.querySelector('.search__input');
-//     const searchBtn = document.querySelector('.search__btn');
-//     searchBtn.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         searchInput.classList.toggle('search--active');
-//     });
-
-//     document.querySelector('.main').addEventListener('click', (e) => {
-//         if (searchInput.classList.contains('search--active')) {
-//             searchInput.classList.remove('search--active');
-//         }
-//     });
-// }
-
 if (document.querySelector('.search--mobile')) {
     const searchMobile = document.querySelector('.search--mobile');
     const search = document.querySelector('.search');
