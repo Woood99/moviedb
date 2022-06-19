138f8208a1717a8bdfada904107f02dd867adbf6
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2022-06-19 22:46:15 +0300

    fix search

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,15 +1,40 @@
-if (document.querySelector('.search')) {
-    const searchForm = document.querySelector('.search');
-    const searchInput = document.querySelector('.search__input');
-    const searchBtn = document.querySelector('.search__btn');
-    searchBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        searchInput.classList.toggle('search--active');
-    });
-    
-    document.querySelector('.main').addEventListener('click', (e) => {
-        if (searchInput.classList.contains('search--active')) {
-            searchInput.classList.remove('search--active');
+import {
+    isMobile,
+    isTablet,
+    isDesktop
+} from '../functions/check-viewport';
+
+
+// if (document.querySelector('.search') && !isMobile()) {
+//     const searchForm = document.querySelector('.search');
+//     const searchInput = document.querySelector('.search__input');
+//     const searchBtn = document.querySelector('.search__btn');
+//     searchBtn.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         searchInput.classList.toggle('search--active');
+//     });
+
+//     document.querySelector('.main').addEventListener('click', (e) => {
+//         if (searchInput.classList.contains('search--active')) {
+//             searchInput.classList.remove('search--active');
+//         }
+//     });
+// }
+
+if (document.querySelector('.search--mobile')) {
+    const searchMobile = document.querySelector('.search--mobile');
+    const search = document.querySelector('.search');
+    searchMobile.addEventListener('click', () => {
+        document.querySelector('.header').classList.remove('burger--active');
+        document.querySelector('[data-menu]').classList.remove('menu--active');
+        document.querySelector('.burger').classList.remove('burger--active');
+        search.classList.toggle('search--active');
+        if (search.classList.contains('search--active')) {
+            document.querySelector('.page__body').classList.add('no-scroll');
+            document.querySelector('.header').classList.add('search--active');
+        } else {
+            document.querySelector('.page__body').classList.remove('no-scroll');
+            document.querySelector('.header').classList.remove('search--active');
         }
-    });
+    })
 }
--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -5,21 +5,21 @@
 
   burger?.addEventListener('click', (e) => {
     document.querySelector('.header').classList.toggle('burger--active');
+    document.querySelector('.header__search').classList.remove('search--active');
+    document.querySelector('.header').classList.remove('search--active');
     burger?.classList.toggle('burger--active');
     menu?.classList.toggle('menu--active');
-
     if (menu?.classList.contains('menu--active')) {
       burger?.setAttribute('aria-expanded', 'true');
       burger?.setAttribute('aria-label', 'Закрыть меню');
       document.querySelector('.page__body').classList.add('no-scroll');
-
     } else {
       burger?.setAttribute('aria-expanded', 'false');
       burger?.setAttribute('aria-label', 'Открыть меню');
       document.querySelector('.page__body').classList.remove('no-scroll');
       document.querySelectorAll('.nav-dropdown').forEach(el => {
-        el.querySelector('.nav-dropdown__title').classList.remove('_active');
-        el.querySelector('.nav-dropdown__content').setAttribute('hidden', '');
+      el.querySelector('.nav-dropdown__title').classList.remove('_active');
+      el.querySelector('.nav-dropdown__content').setAttribute('hidden', '');
       });
     }
   });
